b92575b935c69deb18567efb8af8cd80ddcc7aee
emilebaelde <emilebaelde@hotmail.com>
2018-11-30 17:58:58 +0100

    logo animation

--- js/mijnscript.js
+++ js/mijnscript.js
@@ -25,6 +25,9 @@ $(document).ready(function() {
             }
         })
 
-    })
+    });
+
+
+    document.getElementById("test").setAttribute("d", "M101.5,71.5h61v49.22c-3.66-7.29-10.94-19.48-24.26-30.5C123.96,78.4,109.55,73.57,101.5,71.5z");
 
 });
